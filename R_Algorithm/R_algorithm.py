6966eb37f07632507f7b5278815fd7069d910f94
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 15:06:42 +0100

    added calculating Cmax with q time

--- R_Algorithm/R_algorithm.py
+++ R_Algorithm/R_algorithm.py
@@ -2,7 +2,7 @@ import pandas as pd
 file = open("SCHRAGE1.DAT", "r")
 n = file.readline()
 Cmax = 0
-Qtime = 0  # aktualny czas zakończenia, jeśli bierzemy pod uwagę czas stygnięcia
+Qtime = 0  # aktualny czas zakonczenia, jesli bierzemy pod uwagę czas stygniecia
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
 # print(file.name, " before")
 df = df.sort_values(by=["r"])
@@ -16,7 +16,7 @@ for index, row in df.iterrows():
     Cmax += row['p']
     if Cmax + row['q'] > Qtime:
         Qtime = Cmax + row['q']
-print("Cmax ostygnięty max: ", Qtime)
+print("Cmax (z czasem stygniecia): ", Qtime)
 
 # print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
 
