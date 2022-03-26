9340f4b4d6bd60510a910c216f614baec1edffee
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 14:19:46 +0100

    more folders

--- R_Algorithm/R_algorithm.py
+++ R_Algorithm/R_algorithm.py
@@ -1,5 +1,5 @@
 import pandas as pd
-file = open("SCHRAGE1.DAT", "r")
+file = open("SCHRAGE5.DAT", "r")
 n = file.readline()
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
 print(file.name, " before")
