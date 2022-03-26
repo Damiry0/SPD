79d1122c4a7656708da012ea6bc3e78225f23d0c
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 15:30:04 +0100

    added comments

--- R_Algorithm/R_algorithm.py
+++ R_Algorithm/R_algorithm.py
@@ -7,27 +7,21 @@ Qtime = 0  # aktualny czas zakonczenia, jesli bierzemy pod uwagę czas stygnieci
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
 # print(file.name, " before")
 start = timer()
-
-
-
 df = df.sort_values(by=["r"])
 # print(file.name, " after")
-
-
-#  zliczanie samego Cmax bez czasu stygnięcia
 for index, row in df.iterrows():
     if row['r'] > Cmax:
-        Cmax += row['r']-Cmax
-    Cmax += row['p']
+        Cmax += row['r']-Cmax  # dodawanie ewentualnego czekania na zadanie
+    Cmax += row['p']  # zwiekszanie czasu Cmax o czas wykonywania
     if Cmax + row['q'] > Qtime:
-        Qtime = Cmax + row['q']
+        Qtime = Cmax + row['q']  # najwiekszy aktualnie czas Cmax z czasem stygniecia
 end = timer()
-workTime = end - start
+workTime = end - start  # czas pracy algorytmu w sekundach
 # print(df)
 print("Cmax (z czasem stygniecia): ", Qtime)
 print("Czas w sekundach", workTime)
 print("Czas w mikrosekundach", workTime*1000)
-print(int(n),"    ", workTime*1000)
+print(int(n), "    ", workTime*1000)
 
 # print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
 
