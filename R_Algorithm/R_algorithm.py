168ddb5c6be49ac33e7148b253b8c96984c8f28f
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 15:26:01 +0100

    nice data printing

--- R_Algorithm/R_algorithm.py
+++ R_Algorithm/R_algorithm.py
@@ -1,13 +1,18 @@
 import pandas as pd
-file = open("SCHRAGE1.DAT", "r")
+from timeit import default_timer as timer
+file = open("SCHRAGE9.DAT", "r")
 n = file.readline()
 Cmax = 0
 Qtime = 0  # aktualny czas zakonczenia, jesli bierzemy pod uwagę czas stygniecia
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
 # print(file.name, " before")
+start = timer()
+
+
+
 df = df.sort_values(by=["r"])
 # print(file.name, " after")
-print(df)
+
 
 #  zliczanie samego Cmax bez czasu stygnięcia
 for index, row in df.iterrows():
@@ -16,7 +21,13 @@ for index, row in df.iterrows():
     Cmax += row['p']
     if Cmax + row['q'] > Qtime:
         Qtime = Cmax + row['q']
+end = timer()
+workTime = end - start
+# print(df)
 print("Cmax (z czasem stygniecia): ", Qtime)
+print("Czas w sekundach", workTime)
+print("Czas w mikrosekundach", workTime*1000)
+print(int(n),"    ", workTime*1000)
 
 # print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
 
