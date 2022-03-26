e0533724ba86b960ff09aeaa5db10cb8dd1a4917
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 14:38:01 +0100

    added Cmax calculating

--- R_Algorithm/R_algorithm.py
+++ R_Algorithm/R_algorithm.py
@@ -1,11 +1,17 @@
 import pandas as pd
-file = open("SCHRAGE5.DAT", "r")
+file = open("SCHRAGE1.DAT", "r")
 n = file.readline()
+Cmax = 0
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
-print(file.name, " before")
-print(df)
+# print(file.name, " before")
 df = df.sort_values(by=["r"])
-print(file.name, " after")
+# print(file.name, " after")
 print(df)
-print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
+for index, row in df.iterrows():
+    if row['r'] > Cmax:
+        Cmax += row['r']-Cmax
+    Cmax += row['p']
+
+print("Cmax: ", Cmax)
+# print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
 
