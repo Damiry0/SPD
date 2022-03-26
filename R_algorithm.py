3e4a5c7e4ff3db0a5a27316d73b9f9ba93e84d1a
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 14:14:08 +0100

    added printing latex table to txt

--- R_algorithm.py
+++ R_algorithm.py
@@ -2,6 +2,10 @@ import pandas as pd
 file = open("SCHRAGE1.DAT", "r")
 n = file.readline()
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
+print(file.name, " before")
 print(df)
-df = df.sort_values(by=["r","p"])
+df = df.sort_values(by=["r"])
+print(file.name, " after")
 print(df)
+print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
+
