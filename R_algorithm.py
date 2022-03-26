cb4226217b8ffb5b8fba5302d740000a3652615b
JakubNowek <jakubbnwek@gmail.com>
2022-03-26 13:54:40 +0100

    added reading and sorting by r and p

--- R_algorithm.py
+++ R_algorithm.py
@@ -1,1 +1,7 @@
 import pandas as pd
+file = open("SCHRAGE1.DAT", "r")
+n = file.readline()
+df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
+print(df)
+df = df.sort_values(by=["r","p"])
+print(df)
