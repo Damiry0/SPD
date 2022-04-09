ee8db269bacb9e1399c6c5ad3c8d73b6c7dac3cb
JakubNowek <jakubbnwek@gmail.com>
2022-04-09 11:35:51 +0200

    added schrage algorithm but result not ok

--- Schrage/Schrage.py
+++ Schrage/Schrage.py
@@ -5,10 +5,16 @@ n = file.readline()
 Cmax = 0
 Qtime = 0  # aktualny czas zakonczenia, jesli bierzemy pod uwagę czas stygniecia
 df = pd.read_csv(file, sep=' ', names=['r', 'p', 'q'])
+# r - czas oczekiwania
+# p - czas wykonywania zadania
+# q - czas stygnięcia/dostarczenia
+
 # print(file.name, " before")
+# print(df)
 start = timer()
-df = df.sort_values(by=["r"])
+df = df.sort_values(by=["r", "q"], ascending=[True, False])
 # print(file.name, " after")
+# print(df)
 for index, row in df.iterrows():
     if row['r'] > Cmax:
         Cmax += row['r']-Cmax  # dodawanie ewentualnego czekania na zadanie
@@ -17,6 +23,9 @@ for index, row in df.iterrows():
         Qtime = Cmax + row['q']  # najwiekszy aktualnie czas Cmax z czasem stygniecia
 end = timer()
 workTime = end - start  # czas pracy algorytmu w sekundach
+
+
+
 print(df)
 print("Cmax (z czasem stygniecia): ", Qtime)
 # print("Czas w sekundach", workTime)
@@ -24,4 +33,3 @@ print("Czas w mikrosekundach", workTime*1000)
 # print(int(n), "    ", workTime*1000)
 
 # print(df.to_latex(index=False), file=open("latex_data.txt",'w'))
-
