1a361dac7304411c4f2f0a824e77c544c045cad5
JakubNowek <jakubbnwek@gmail.com>
2022-04-10 15:20:10 +0200

    ready to fool drag

--- Schrage/Schrage.py
+++ Schrage/Schrage.py
@@ -1,6 +1,12 @@
 from timeit import default_timer as timer
 
-file = open("Data/SCHRAGE2.DAT", "r")
+
+def display(List):
+    for (a, b, c, d) in List:
+        print("i:", d + 1, "( r:", a, "p:", b, "q:", c, ")")
+
+
+file = open("Data/skoczylas.dat", "r")
 n = file.readline()
 lines = file.readlines()
 data = [tuple(int(x) for x in line.strip().split()) for line in lines]
@@ -8,43 +14,13 @@ data1 = []
 for i in range(0, len(data)):
     data1.append((data[i][0], data[i][1], data[i][2], i))
 
-start = timer()
-for (a, b, c, d) in data1:
-    print("i:", d + 1, "( r:", a, "p:", b, "q:", c, ")")
-
-count = 0
-tmp_count = 0
+# start = timer()
+display(data1)
 resultList = []
-
-# while len(data1) > 0:
-#     data1.sort(key=lambda x: x[0], reverse=True)
-#     popped = data1.pop()
-#     tmp_array = [popped]
-#     for item in data1:
-#         if popped[0] == item[0]:
-#             tmp_array.append(item)
-#             data1.remove(item)
-#     tmp_array.sort(key=lambda x: x[2])
-#     count += tmp_array[0][1] + tmp_array[0][0]
-#     sortedArray.append(tmp_array.pop())
-#     if len(tmp_array) != 0:
-#         data1.extend(tmp_array)
-#     tmp_array=[]
-#     data1.sort(key=lambda x: x[2],reverse=True)
-#     for item2 in data1:
-#         if count >= item2[0]:
-#             tmp_array.append(item2)
-#     tmp_array.sort(key=lambda x: x[2])
-#     while tmp_array:
-#         popped=tmp_array.pop()
-#         tmp_count += popped[1]  # dodatkowe warunki
-#         sortedArray.append(popped)
-#         data1.remove(popped)
-#     count += tmp_count
 Cmax = 0
 Qtime = 0
 firstScan = True
-# to perwsze wybranie r musi sie wykontywac tylko raz a potem to juz nie
+# to pierwsze wybranie r musi sie wykonywac tylko raz, a potem to juz nie
 waitingList = sorted(data1, key=lambda x: (x[0], -x[2]))
 while len(waitingList) > 0:
     waitingList = sorted(waitingList, key=lambda x: (x[0], -x[2]))
@@ -65,15 +41,14 @@ while len(waitingList) > 0:
         if Cmax >= item[0]:
             resultList.append(item)
             Cmax += item[1]  # zwiekszanie czasu Cmax o czas wykonywania
-            print("Cmax petla=", Cmax)
+            # print("Cmax petla=", Cmax)
             if Cmax + item[2] > Qtime:
                 Qtime = Cmax + item[2]  # najwiekszy aktualnie czas Cmax z czasem stygniecia
             tmp_array.remove(item)
     waitingList = tmp_array.copy()
 # end = timer()
-print("Qtime=", Qtime)
+print("Cmax (z czasem stygniecia): ", Qtime)
 print("Result")
-for (a, b, c, d) in resultList:
-    print("i:", d + 1, "( r:", a, "p:", b, "q:", c, ")")
+display(resultList)
 # print("Time elapsed:", end - start)
 file.close()
